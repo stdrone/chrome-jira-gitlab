ed1631ab51be5219b495b4b721fcca5e9dae66aa
Andrey Kozeletskiy <stdrone@ya.ru>
2021-12-12 22:15:56 +0300

    popup

--- chrome-jira-gitlab/src/popup/App.vue
+++ chrome-jira-gitlab/src/popup/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="popup" />
+  <div class="popup">
+    <div v-if="url">
+      {{ url }}
+    </div>
+    <div class="noconfig" v-else>No config for this site</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  props: {
+    url: String,
+    config: Object,
+  },
+  mounted() {
+    var query = { active: true, currentWindow: true };
+    const me = this;
+    chrome.tabs.query(query, (tabs) => {
+      let url = tabs[0].url;
+      me.$store.getters.configData.forEach((element) => {
+        if (url.includes(element.jira)) {
+          me.url = url;
+          me.config = element;
+        }
+      });
+    });
+  },
 };
 </script>
 
@@ -12,5 +34,9 @@ export default {
 html {
   width: 400px;
   height: 400px;
+}
+
+.noconfig {
+  text-align: center;
 }
 </style>
